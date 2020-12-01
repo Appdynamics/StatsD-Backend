756a471dc58c27b9b7d151da94a486b15e47b632
David Ryder <38921518+APPDRYDER@users.noreply.github.com>
2020-12-01 08:27:08 -0800

    Update envvars.appdynamics.sh

--- envvars.appdynamics.sh
+++ envvars.appdynamics.sh
@@ -5,5 +5,7 @@ export APPDYNAMICS_CONTROLLER_HOST_NAME=
 export APPDYNAMICS_SERVERLESS_API_ENDPOINT=https://pdx-sls-agent-api.saas.appdynamics.com
 export APPDYNAMICS_CONTROLLER_PORT=443
 export APPDYNAMICS_CONTROLLER_SSL_ENABLED="true"
+# Server Visibility (SIM) is required
 export APPDYNAMICS_SIM_ENABLED="true"
+# Modify to reflect the Machine Agent Bundle Zip file used
 export APPDYNAMICS_MACHINE_AGENT_ZIP_FILE="machineagent-bundle-64bit-linux-4.5.18.2430.zip"
